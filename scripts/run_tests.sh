bd867b559b19ff69a16a00cc91e23861f28e0eeb
Finn Ebeling <finn.ebeling@stud.hs-flensburg.de>
2023-05-16 15:29:13 +0200

    Update test-script and remove build_runner

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,18 +4,10 @@ function run_flutter_test() {
     flutter test test
 }
 
-function check_and_run_build_runner() {
-    local pubspec_file="$1"
-    if grep -q "build_runner:" "$pubspec_file"; then
-        dart run build_runner build
-    fi
-}
-
 function process_directory() {
     local dir="$1"
     if [ -f "$dir/pubspec.yaml" ]; then
         cd "$dir"
-        check_and_run_build_runner "pubspec.yaml"
         run_flutter_test
         cd ..
     fi
@@ -24,7 +16,6 @@ function process_directory() {
 cd ..
 
 # Tests im root verzeichnis starten
-check_and_run_build_runner "pubspec.yaml"
 run_flutter_test
 
 # In das 'packages'-Verzeichnis wechseln
