76b1f944f684150737f424a0a6bbe8cf953ff832
Leon Stiller <leon.stiller@stud.hs-flensburg.de>
2023-05-25 13:57:25 +0200

    participants UI first design

--- lib/courses/details/participants/view/course_participants_page.dart
+++ lib/courses/details/participants/view/course_participants_page.dart
@@ -1,3 +1,5 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:studipadawan/courses/details/participants/bloc/course_participants_bloc.dart';
@@ -9,7 +11,35 @@ class CourseParticipantsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => CourseParticipantsBloc(),
-      child: const Text('Teilnehmer'),
+      child: ListView.separated(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: const Text('Max Mustermann'),
+              subtitle: const Text('max@mustermann.de'),
+              leading: CircleAvatar(
+                backgroundColor:
+                    Theme.of(context).primaryColor.withOpacity(0.1),
+                child: Padding(
+                  padding: const EdgeInsets.all(AppSpacing.xxs),
+                  child: Text(
+                    'M',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {}, icon: const Icon(EvaIcons.emailOutline)),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider(
+              height: 1,
+              indent: AppSpacing.xxxlg,
+            );
+          },
+          itemCount: 5),
     );
   }
 }
