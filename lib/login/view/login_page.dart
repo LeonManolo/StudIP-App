ecf53338322d6a0e37deddcdc94f7cf40edd1b92
Leon Stiller <leon.stiller@stud.hs-flensburg.de>
2023-05-27 13:55:56 +0000

    SFP-61 Authentication: Login Screen

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -1,7 +1,15 @@
+import 'package:app_ui/app_ui.dart';
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:studipadawan/app/bloc/app_bloc.dart';
 import 'package:studipadawan/login/cubit/login_cubit.dart';
+import 'package:studipadawan/login/cubit/login_state.dart';
+import 'package:studipadawan/login/widgets/login_button.dart';
+import 'package:studipadawan/login/widgets/login_illustration.dart';
+import 'package:studipadawan/utils/loading_indicator.dart';
+import 'package:studipadawan/utils/utils.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -10,54 +18,80 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.download),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.close),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: BlocProvider(
-          create: (_) => LoginCubit(context.read<AuthenticationRepository>()),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _StudIpLoginButton(),
-            ],
-          ),
-        ),
+    return BlocProvider(
+      create: (_) => LoginCubit(context.read<AuthenticationRepository>()),
+      child: BlocConsumer<LoginCubit, LoginState>(
+        listener: (context, state) {
+          if (state == LoginState.failure) {
+            buildSnackBar(context, 'Fehler beim Anmelden', null);
+          }
+        },
+        builder: (context, state) {
+          switch (state) {
+            case LoginState.inProgress:
+              return const Material(child: LoadingIndicator());
+
+            case _:
+              return Scaffold(
+                backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+                appBar: AppBar(
+                  actions: [
+                    IconButton(
+                      onPressed: () {
+                        context.read<AppBloc>().add(const AppLogoutRequested());
+                      },
+                      icon: const Icon(EvaIcons.logOutOutline,
+                        color: Colors.transparent,
+                      ),
+                    )
+                  ],
+                ),
+                body: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      const LoginIllustration(),
+                      Text(
+                        'Anmelden',
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w600,
+                            ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: AppSpacing.md,
+                          bottom: AppSpacing.xxlg,
+                        ),
+                        child: Text(
+                          'Melde dich jetzt mit deinem\nStudIP Account an',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Theme.of(context).hintColor,
+                          ),
+                        ),
+                      ),
+                      LoginButton(
+                        onPressed: () => context.read<LoginCubit>().loginWithStudIp(),
+                      ),
+                      const Spacer(),
+                      const SafeArea(child: Padding(
+                        padding: EdgeInsets.all(AppSpacing.lg),
+                        child: Text('Vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames',
+                        textAlign: TextAlign.center,),
+                      ),),
+                      //_StudIpLoginButton(),
+                    ],
+                  ),
+                ),
+              );
+          }
+        },
       ),
     );
   }
 }
 
-class _StudIpLoginButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return ElevatedButton.icon(
-      key: const Key('loginForm_googleLogin_raisedButton'),
-      label: const Text(
-        'SIGN IN WITH STUD IP',
-        style: TextStyle(color: Colors.white),
-      ),
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-        backgroundColor: theme.colorScheme.secondary,
-      ),
-      icon: const Icon(Icons.person, color: Colors.white),
-      onPressed: () => context.read<LoginCubit>().loginWithStudIp(),
-    );
-  }
-}
+
+
+
---
+++ lib/login/widgets/login_button.dart
@@ -0,0 +1,42 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({
+    super.key,
+    this.onPressed,
+  });
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FilledButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+        child: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                right: AppSpacing.sm,
+                top: AppSpacing.md,
+                bottom: AppSpacing.md,
+              ),
+              child: Icon(EvaIcons.externalLinkOutline),
+            ),
+            Text('Mit Stud.IP anmelden'),
+          ],
+        ),
+      ),
+    );
+  }
+}
