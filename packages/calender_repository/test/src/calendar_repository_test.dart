f66b2d40d4e4f319f9b6be77571c4b4f76d278f9
Finn Ebeling <finn.ebeling@stud.hs-flensburg.de>
2023-06-06 17:07:24 +0200

    wip

---
+++ packages/calender_repository/test/src/calendar_repository_test.dart
@@ -0,0 +1,6 @@
+import 'package:calender_repository/calender_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:studip_api_client/studip_api_client.dart';
+
+void main() {}
