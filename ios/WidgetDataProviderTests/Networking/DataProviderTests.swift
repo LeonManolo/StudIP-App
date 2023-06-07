bffe494bc1457fd3916cf1880740417c91aa701d
Finn Ebeling <finn.ebeling@stud.hs-flensburg.de>
2023-06-07 11:30:51 +0200

    Fix native iOS Tests

--- ios/WidgetDataProviderTests/Networking/DataProviderTests.swift
+++ ios/WidgetDataProviderTests/Networking/DataProviderTests.swift
@@ -21,41 +21,45 @@ final class DataProviderTests: XCTestCase {
     
     // MARK: - loadRemoteScheduleItems
     
-    func test_loadRemoteScheduleItems_returnScheduleEntry() async throws {
+    func test_loadRemoteScheduleItems_returnScheduleEntries() async throws {
         setupSut()
 
         let date = try XCTUnwrap(Calendar.german.date(from: DateComponents(year: 2023, month: 5, day: 15, hour: 14, minute: 0)))
         let scheduleItems = try await sut.loadRemoteScheduleItems(for: date)
 
+        XCTAssertEqual(scheduleItems.count, 2)
         XCTAssertEqual(scheduleItems[0].title, "Vorlesung 1")
         XCTAssertEqual(scheduleItems[1].title, "Eintrag 2")
     }
     
-    func test_loadRemoteScheduleItems_returnNoScheduleEntry_tooLate() async throws {
+    func test_loadRemoteScheduleItems_returnOnlyOneScheduleEntry_tooLate() async throws {
         setupSut()
 
         let date = try XCTUnwrap(Calendar.german.date(from: DateComponents(year: 2023, month: 5, day: 15, hour: 14, minute: 1)))
         let scheduleItems = try await sut.loadRemoteScheduleItems(for: date)
 
-        XCTAssertTrue(scheduleItems.isEmpty)
+        XCTAssertEqual(scheduleItems.count, 1)
+        XCTAssertEqual(scheduleItems[0].title, "Eintrag 2")
     }
     
-    func test_loadRemoteScheduleItems_returnNoScheduleEntry_excludedDate() async throws {
+    func test_loadRemoteScheduleItems_returnOnlyOneScheduleEntry_excludedDate() async throws {
         setupSut()
 
         let date = try XCTUnwrap(Calendar.german.date(from: DateComponents(year: 2023, month: 5, day: 1, hour: 14, minute: 0)))
         let scheduleItems = try await sut.loadRemoteScheduleItems(for: date)
 
-        XCTAssertTrue(scheduleItems.isEmpty)
+        XCTAssertEqual(scheduleItems.count, 1)
+        XCTAssertEqual(scheduleItems[0].title, "Eintrag 2")
     }
     
-    func test_loadRemoteScheduleItems_returnNoScheduleEntry_2WeekInterval() async throws {
+    func test_loadRemoteScheduleItems_returnOnlyOneScheduleEntry_2WeekInterval() async throws {
         setupSut()
 
         let date = try XCTUnwrap(Calendar.german.date(from: DateComponents(year: 2023, month: 6, day: 19, hour: 14, minute: 0)))
         let scheduleItems = try await sut.loadRemoteScheduleItems(for: date)
 
-        XCTAssertTrue(scheduleItems.isEmpty)
+        XCTAssertEqual(scheduleItems.count, 1)
+        XCTAssertEqual(scheduleItems[0].title, "Eintrag 2")
     }
 }
 
