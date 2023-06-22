97d2bfee4d9fb3e49b69e139fa91a7e7532322f9
Leon Stiller <leon.stiller@stud.hs-flensburg.de>
2023-06-22 10:36:08 +0200

    added error state widget

---
+++ lib/utils/widgets/error_view/error_illustration.dart
@@ -0,0 +1,34 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+
+class ErrorIllustration extends StatelessWidget {
+  const ErrorIllustration({
+    super.key,
+    this.color,
+    required this.iconData,
+  });
+
+  final Color? color;
+  final IconData iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(AppSpacing.xlg),
+      margin: const EdgeInsets.only(
+        top: AppSpacing.xxxlg + AppSpacing.xxlg,
+        bottom: AppSpacing.xxlg,
+      ),
+      decoration: BoxDecoration(
+        color: (color ?? Colors.red).withOpacity(0.1),
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+      ),
+      child: Icon(
+        iconData,
+        size: MediaQuery.of(context).size.width * 0.18,
+        color: color ?? Colors.red,
+      ),
+    );
+  }
+}
