6d15f99174bd810b68a418df657fc2334eb21bc1
aljoscha <alboe1994@gmail.com>
2023-06-29 16:19:47 +0200

    formatting

--- lib/courses/details/files/view/widgets/course_files_path_list.dart
+++ lib/courses/details/files/view/widgets/course_files_path_list.dart
@@ -9,15 +9,14 @@ class CourseFilesPathRowList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CourseFilesBloc, CourseFilesState>(
-      builder: (context, state) {   
+      builder: (context, state) {
         return SizedBox(
           height: 40,
           child: ListView.separated(
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
               final folderInfo = state.parentFolders.elementAt(index);
-              return 
-              TextButton(
+              return TextButton(
                 style: TextButton.styleFrom(
                   minimumSize: const Size.fromWidth(20),
                 ),
